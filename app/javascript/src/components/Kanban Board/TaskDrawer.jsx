10693be944b102e944ca843178c4dad5ec512d48
Abhiram Pai <abhirampai1999@gmail.com>
2023-07-24 12:11:09 +0530

    Added breadcrumb to show drawer

--- app/javascript/src/components/Kanban Board/TaskDrawer.jsx
+++ app/javascript/src/components/Kanban Board/TaskDrawer.jsx
@@ -1,16 +1,18 @@
 /* eslint-disable import/order */
 import React from "react";
 
-import { Drawer, Spin } from "antd";
+import { Breadcrumb, Button, Drawer, Spin } from "antd";
 
 import TaskForm from "components/Kanban Board/TaskForm";
 import { useFetchIssue, useDestroyIssue } from "hooks/useIssues";
+import useIssueStore from "hooks/useIssueStore";
 import { DeleteOutlined } from "@ant-design/icons";
 
 const TaskDrawer = ({ onClose, issueId }) => {
   const { mutateAsync: destroyIssue } = useDestroyIssue();
 
   const { data, isLoading, isFetching } = useFetchIssue(issueId);
+  const setIssueId = useIssueStore(state => state.setIssueId);
 
   const issue = data?.data;
 
@@ -18,12 +20,40 @@ const TaskDrawer = ({ onClose, issueId }) => {
     destroyIssue(issueId, { onSuccess: onClose() });
   };
 
+  const breadcrumbItems = () => {
+    const breadcrumb = [];
+
+    if (issue?.parent_id) {
+      breadcrumb.push({
+        title: (
+          <Button className="p-0 h-auto" type="link">
+            {issue.parent_display_name}
+          </Button>
+        ),
+        onClick: () => setIssueId(issue.parent_id),
+      });
+    }
+
+    breadcrumb.push({
+      title: issue.display_name,
+    });
+
+    return breadcrumb;
+  };
+
+  const renderHeader = () =>
+    isLoading || isFetching ? (
+      "Issue"
+    ) : (
+      <Breadcrumb items={breadcrumbItems()} />
+    );
+
   return (
     <Drawer
       extra={<DeleteOutlined onClick={deleteIssue} />}
       open={issueId}
       placement="right"
-      title={issue?.display_name || "Issue"}
+      title={renderHeader()}
       onClose={onClose}
     >
       {isLoading || isFetching ? (
--- app/views/api/v1/issues/show.json.jbuilder
+++ app/views/api/v1/issues/show.json.jbuilder
@@ -8,3 +8,18 @@ json.extract! @issue,
   :description,
   :display_name,
   :owner_name
+
+if @issue.parent_issue.present?
+  json.parent_id @issue.parent_issue_id
+  json.parent_display_name @issue.parent_display_name
+end
+
+if @issue.sub_issues.present?
+  json.sub_issues @issue.sub_issues do |sub_issue|
+    json.extract! sub_issue,
+      :id,
+      :summary,
+      :priority,
+      :issue_type
+  end
+end
